6f967a47c46571b62ad7a6f0304bb51a690c9e58
SSPadmaCharan <padmacharankdm3024@gmail.com>
2026-07-03 23:46:07 +0530

    detailed description for each code

--- 01_basic_syntax/simple_wire.v
+++ 01_basic_syntax/simple_wire.v
@@ -1,7 +1,29 @@
-module simple_wire(  
-      input in,
+
+// Module Name : simple_wire
+// Author      : Padma Charan S S
+// Repository  : Verilog_Fundamentals
+// Folder      : 01_basic_syntax
+//
+// Description:
+// A simple wire connection.
+// The output continuously follows the input.
+//
+// Function:
+// out = in
+//
+// Inputs:
+// in  - 1-bit input signal
+//
+// Outputs:
+// out - 1-bit output signal
+//
+
+
+module simple_wire(
+    input in,
     output out
 );
 
-assign out = in ;
-endmodule 
+assign out = in;
+
+endmodule
