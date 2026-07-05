8b4b582f00909e5581744141d6e6ba9afc35f516
SSPadmaCharan <padmacharankdm3024@gmail.com>
2026-07-05 23:29:25 +0530

    nxt pr

---
+++ 02_logic_gates/06_xor_gate.v
@@ -0,0 +1,33 @@
+//==============================================================
+// Project    : XOR Gate using Verilog
+// Description: Implements a 2-input XOR (Exclusive OR) gate.
+//
+// An XOR gate has two inputs and one output.
+// The output becomes HIGH (1) only when the inputs are DIFFERENT.
+// If both inputs are the same (both 0 or both 1),
+// the output becomes LOW (0).
+//
+// Boolean Expression:
+//      y = a ^ b
+//
+// Truth Table:
+//      a | b | y
+//     -----------
+//      0 | 0 | 0
+//      0 | 1 | 1
+//      1 | 0 | 1
+//      1 | 1 | 0
+//
+// '^' is the Verilog XOR (Exclusive OR) operator.
+//
+//==============================================================
+
+
+
+module xopr(
+    input a,
+    input b,
+    output y
+);
+assign y = a^b;
+endmodule
