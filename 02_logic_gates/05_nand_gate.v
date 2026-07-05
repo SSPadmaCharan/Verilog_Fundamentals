3c289420b248a945fb8c981addcdfc0571b66380
SSPadmaCharan <padmacharankdm3024@gmail.com>
2026-07-05 23:19:06 +0530

    nxt pr

---
+++ 02_logic_gates/05_nand_gate.v
@@ -0,0 +1,32 @@
+
+//==============================================================
+// Project    : NAND Gate using Verilog
+// Description: Implements a 2-input NAND gate.
+//
+// A NAND gate has two inputs and one output.
+// The output becomes LOW (0) only when BOTH inputs are HIGH (1).
+// Otherwise, the output remains HIGH (1).
+//
+// Boolean Expression:
+//      y = ~(a & b)
+//
+// Truth Table:
+//      a | b | y
+//     -----------
+//      0 | 0 | 1
+//      0 | 1 | 1
+//      1 | 0 | 1
+//      1 | 1 | 0
+//
+// '&' is the Verilog AND operator.
+// '~' is the Verilog NOT operator.
+// Together, '~(a & b)' performs the NAND operation.
+//
+//==============================================================
+module nandgate (
+    input a,
+    input b,
+    output y
+);
+assign y = ~a+~b;
+endmodule
